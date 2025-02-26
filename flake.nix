d8790c35c7e713f3186542592c4bbbd383e6e6f2
Pim Snel <post@pimsnel.com>
2025-02-26 15:02:50 +0100

    override quarto yeahman

--- flake.nix
+++ flake.nix
@@ -5,11 +5,33 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+  let
 
-    packages.x86_64-linux.quarto = nixpkgs.legacyPackages.x86_64-linux.quarto;
+    system         = "x86_64-linux";
+      #old-quarto-pkgs = nixpkgs.legacyPackages.${system};
+    quarto-overlay  = final: prev: {
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.quarto;
+      quarto = prev.quarto.override {
+        extraRPackages = [
+          prev.rPackages.reticulate
+        ];
+        extraPythonPackages = ps: with ps; [
+          plotly
+          numpy
+          pandas
+          matplotlib
+          tabulate
+        ];
+      };
+    };
 
+    pkgs  = import nixpkgs { inherit system; overlays = [ quarto-overlay ];};
+
+
+  in
+  {
+    packages.x86_64-linux.quarto = pkgs.quarto;
+    packages.x86_64-linux.default = pkgs.quarto;
   };
 }
