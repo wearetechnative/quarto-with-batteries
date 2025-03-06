1dd1b83f8715dbf4be2e68b0a299bc39721b8f86
Pim Snel <post@pimsnel.com>
2025-03-06 17:44:25 +0100

    fix missing paths

--- flake.nix
+++ flake.nix
@@ -31,10 +31,28 @@
   {
     packages.x86_64-linux.quarto = pkgs.quarto;
     packages.x86_64-linux.default = pkgs.quarto;
+
     packages.x86_64-linux.quarto-for-quiqr =
-      pkgs.writeShellScriptBin "quarto-for-quiqr" ''
-        ${pkgs.quarto}/bin/quarto render "''${1}"
-        echo "''${1%.*}".pdf
-      '';
+      let
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+        qname = "q4q";
+        qscript = pkgs.writeShellScriptBin qname ''
+            ${pkgs.quarto}/bin/quarto render "''${1}"
+            echo "''${1%.*}".pdf
+        '';
+        qBuildInputs = with pkgs; [ quarto ];
+
+      in pkgs.symlinkJoin {
+        name = qname;
+        paths = [ qscript ] ++ qBuildInputs;
+        buildInputs = [ pkgs.makeWrapper ];
+        postBuild = "wrapProgram $out/bin/${qname} --prefix PATH : $out/bin";
+      };
+
+      #    packages.x86_64-linux.quarto-for-quiqr2 =
+      #      pkgs.writeShellScriptBin "quarto-for-quiqr2" ''
+      #        ${pkgs.quarto}/bin/quarto render "''${1}"
+      #        echo "''${1%.*}".pdf
+      #      '';
   };
 }
